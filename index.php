58d73cec147255cead6e7a9e68226a5158d0ecd5
Egor Embrekht <embre@bk.ru>
2025-06-25 09:24:56 +0000

    Added loyality points display

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle('Главная');
-
-if ($USER->IsAuthorized())
-	LocalRedirect("/profile/");
-else
-	LocalRedirect("/auth/");
-?> 
-
+LocalRedirect("/auth/");
+?>
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
--- profile/index.php
+++ profile/index.php
@@ -1,4 +1,5 @@
 <?
+// CModule::IncludeModule("main");
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Title");
 ?>
@@ -6,9 +7,13 @@ $APPLICATION->SetTitle("Title");
 if (!$USER->IsAuthorized()) {
     LocalRedirect("/"); 
 }
+
+$userId = $USER->GetID(); 
+$rsUser = CUser::GetByID($userId);
+$arUser = $rsUser->Fetch();
 ?>
 
-<?$APPLICATION->IncludeComponent(
+<!-- <?$APPLICATION->IncludeComponent(
     "bitrix:main.profile",
     ".default", 
     Array(
@@ -18,6 +23,9 @@ if (!$USER->IsAuthorized()) {
         "CHECK_RIGHTS" => "N", 
         "USER_PROPERTY_NAME" => "",
     )
-);?>
+);?> -->
+
+<h1>Итого баллов у текущего пользователя (id: <?= $USER->GetID() ?>): <?= $arUser['UF_LOYALITY_POINTS'];?></h1>
+
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
