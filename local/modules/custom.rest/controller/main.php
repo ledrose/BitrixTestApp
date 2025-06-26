99e6bf8c9705ea3b30a07784a9dcdf55c308510b
Egor Embrekht <embre@bk.ru>
2025-06-26 07:28:24 +0000

    Api routes

--- local/modules/custom.rest/controller/main.php
+++ local/modules/custom.rest/controller/main.php
@@ -1,8 +1,13 @@
 <?
 namespace Custom\Rest\Controller;
 
+use Bitrix\Iblock\IblockTable;
 use Bitrix\Main\Engine\Controller;
 use Bitrix\Main\Engine\ActionFilter;
+use CIBlock;
+use CIBlockElement;
+use CModule;
+use CUser;
 
 class RestController extends Controller
 {
@@ -16,23 +21,64 @@ class RestController extends Controller
     public function configureActions()
     {
         return [
-            'test' => [
-                'prefilters' => [
-                    new ActionFilter\HttpMethod(['GET'])
-                ]
-            ]
+            // Также без авторизации, ограничения на методы и т.д.
         ];    
     }
 
-    public function testAction(string $param1 = 'No_param', int $param2 = 0)
+
+    private static function validateNumber(?int $number)
     {
-        $data = [
-            'message' => 'Hello world!',
-            'receivedParam1' => htmlspecialcharsbx($param1),
-            'receivedParam2' => $param2,
-            'timestamp' => time(),
-        ];
-        return $data;
+        if (!isset($number) || !is_numeric($number) || $number < 0) {
+            throw new \Exception("Неверное число");
+        }
+        return true;
     }
+
+    // Получение баллов пользователя
+    public function getUserLoyaltyPointsAction(int $user_id)
+    {   
+        if (!static::validateNumber($user_id)) {
+            throw new \Exception("Неверный ID пользователя");
+        }
+        $user = CUser::GetByID($user_id)->Fetch();
+        return $user['UF_LOYALITY_POINTS'];
+    }
+
+
+    public function getUserLoyaltyPointsHistoryAction(int $user_id)
+    {
+        $request = \Bitrix\Main\Context::getCurrent()->getRequest();
+        $limit = (int)$request->getQuery("limit") ?: 10;
+        $offset = (int)$request->getQuery("offset") ?: 0;
+        echo $limit . " " . $offset;
+        if (!static::validateNumber($user_id) || !static::validateNumber($limit) || !static::validateNumber($offset)) {
+            throw new \Exception("Неверное свойство параметра");
+        }
+        if (!CModule::IncludeModule('iblock')) {
+            throw new \Exception("Модуль iblock не установлен");
+        }
+        // Попытался использовать IblockTable::GetList, но я не нашел информации о том, как отфильтровать по свойстам (PROPERTY_USER_ID)
+        $arOrder = [
+            'DATE_ACTIVE_FROM' => 'DESC',
+        ];
+        $arFilter = [
+            "IBLOCK_TYPE_ID" => 1,
+            'PROPERTY_USER_ID' => $user_id,
+        ];
+        $arSelect = [
+            'PROPERTY_USER_ID', 'DATE_ACTIVE_FROM', 'PROPERTY_AMOUNT',
+        ];
+        $arNavStartParams = [
+            'nTopCount' => $limit,
+            'nOffset' => $offset,
+        ];
+        $rsIBlocks = CIBlockElement::GetList($arOrder,$arFilter,false,$arNavStartParams,$arSelect);
+        $arIBlocks = [];
+        while ($arIBlock = $rsIBlocks->Fetch()) {
+            $arIBlocks[] = $arIBlock;
+        }
+        return $rsIBlocks;
+    }
+
 }
 
--- local/routes/api.php
+++ local/routes/api.php
@@ -1,7 +1,17 @@
 <?php
 
+use Custom\Rest\Controller\RestController;
 CModule::IncludeModule('custom.rest');
 
+
+// Prefix не работает по какой-то причине не рабоатет с несколькими '/' и его нельзя вызывать несколько раз (prefix()->prefix())
 return function (\Bitrix\Main\Routing\RoutingConfigurator $routes) {
-    $routes->get('/stuff/user/', [Custom\Rest\Controller\RestController::class, 'testAction']);
+    $routes->prefix('api')->group( function (\Bitrix\Main\Routing\RoutingConfigurator $routes) {
+        $routes->prefix('v1')->group( function (\Bitrix\Main\Routing\RoutingConfigurator $routes) {
+            $routes->prefix('user')->group( function (\Bitrix\Main\Routing\RoutingConfigurator $routes) {    
+                $routes->get('loyalty-points/{user_id}', [RestController::class, 'getUserLoyaltyPointsAction']);
+                $routes->get('loyalty-points-history/{user_id}', [RestController::class, 'getUserLoyaltyPointsHistoryAction']);
+            });
+        });
+    });
 };
